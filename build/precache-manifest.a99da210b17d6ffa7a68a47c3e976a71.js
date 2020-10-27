b0deea777cc1f0d65d6ad5563dc6279f856b5ab7
Greg Pfaff <greg.pfaff@silvercar.com>
2020-10-27 14:53:19 -0500

    add build

---
+++ build/precache-manifest.a99da210b17d6ffa7a68a47c3e976a71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b965fcf281ed2a6b096e02164f6a32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "390e2c82883d8c201503",
+    "url": "/static/css/main.3db54d2f.chunk.css"
+  },
+  {
+    "revision": "b427ac7bec136813d7aa",
+    "url": "/static/js/2.6e51ae41.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.6e51ae41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "390e2c82883d8c201503",
+    "url": "/static/js/main.566be537.chunk.js"
+  },
+  {
+    "revision": "879faf00ec9f2137dd2f",
+    "url": "/static/js/runtime-main.570a36cc.js"
+  }
+]);
